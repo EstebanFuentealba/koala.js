32063a17118566bc32568685fee02ed86da341d7
Esteban Fuentealba <mi.warezx@gmail.com>
2012-03-27 02:15:14 -0300

    - Comentada la idea y agregado metodos para formularios: loadRecord, getRecord

--- api.php
+++ api.php
@@ -1,9 +1,10 @@
 <?PHP
-	echo json_encode(array(
-		'items'=> array(
-			array('firstname' => 'Juan',	'lastname' => 'Perez'),
-			array('firstname' => 'Esteban',	'lastname' => 'Fuentealba')
-		)
-	));
+
+echo json_encode(array(
+    'items' => array(
+        array('id_usuario' => 'juperez','firstname' => 'Juan', 'lastname' => 'Perez'),
+        array('id_usuario' => 'esfuentealba','firstname' => 'Esteban', 'lastname' => 'Fuentealba')
+    )
+));
 
 ?>
