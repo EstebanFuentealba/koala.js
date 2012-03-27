8a75147eaa6688837eaa3e9491ccae975d38486e
Esteban Fuentealba <mi.warezx@gmail.com>
2012-03-27 01:41:42 -0300

    - Agregado primer codigo de idea

---
+++ api.php
@@ -0,0 +1,9 @@
+<?PHP
+	echo json_encode(array(
+		'items'=> array(
+			array('firstname' => 'Juan',	'lastname' => 'Perez'),
+			array('firstname' => 'Esteban',	'lastname' => 'Fuentealba')
+		)
+	));
+
+?>
