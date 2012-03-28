371c25943d550091e89473e3746590cb1224d733
Esteban Fuentealba <mi.warezx@gmail.com>
2012-03-27 22:30:28 -0300

    - Agregado Rest, require

--- api.php
+++ api.php
@@ -1,10 +1,21 @@
 <?PHP
+$method = $_SERVER['REQUEST_METHOD'];
+switch ($method) {
+  case 'PUT':
+    break;
+  case 'POST': 
+    break;
+  case 'GET':
+      echo json_encode(array(
+        'items' => array(
+            array('id_usuario' => 'juperez','firstname' => 'Juan', 'lastname' => 'Perez'),
+            array('id_usuario' => 'esfuentealba','firstname' => 'Esteban', 'lastname' => 'Fuentealba')
+        )
+    ));
+    break;
+  case 'DELETE': 
+    break;
+}
 
-echo json_encode(array(
-    'items' => array(
-        array('id_usuario' => 'juperez','firstname' => 'Juan', 'lastname' => 'Perez'),
-        array('id_usuario' => 'esfuentealba','firstname' => 'Esteban', 'lastname' => 'Fuentealba')
-    )
-));
 
 ?>
