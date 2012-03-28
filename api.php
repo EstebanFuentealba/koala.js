d4936d39d8bd9e26936f825df74867107ea921ed
Esteban Fuentealba <mi.warezx@gmail.com>
2012-03-28 13:45:46 -0300

    - Agregado metodos para Form y Combobox - Modificada estructura del plugin - agregado webservice de comuna

--- api.php
+++ api.php
@@ -8,8 +8,8 @@ switch ($method) {
   case 'GET':
       echo json_encode(array(
         'items' => array(
-            array('id_usuario' => 'juperez','firstname' => 'Juan', 'lastname' => 'Perez'),
-            array('id_usuario' => 'esfuentealba','firstname' => 'Esteban', 'lastname' => 'Fuentealba')
+            array('id_usuario' => 'juperez','firstname' => 'Juan', 'lastname' => 'Perez', 'comuna' => 4),
+            array('id_usuario' => 'esfuentealba','firstname' => 'Esteban', 'lastname' => 'Fuentealba', 'comuna' => 2)
         )
     ));
     break;
---
+++ ciudad.php
@@ -0,0 +1,39 @@
+<?php
+
+echo json_encode(
+        array(
+            'items' => array(
+                array(
+                    "id_comuna" => 0,
+                    "nombre_comuna" => "Concepción",
+                    "prc_info" => 50
+                ),
+                array(
+                    "id_comuna" => 2,
+                    "nombre_comuna" => "Talcahuano",
+                    "prc_info" => 40
+                ),
+                array(
+                    "id_comuna" => 3,
+                    "nombre_comuna" => "Chiguayante",
+                    "prc_info" => 50
+                ),
+                array(
+                    "id_comuna" => 4,
+                    "nombre_comuna" => "San Pedro de la Paz",
+                    "prc_info" => 50
+                ),
+                array(
+                    "id_comuna" => 5,
+                    "nombre_comuna" => "Hualpén",
+                    "prc_info" => 50
+                ),
+                array(
+                    "id_comuna" => 6,
+                    "nombre_comuna" => "Hualqui",
+                    "prc_info" => 50
+                )
+            )
+        )
+);
+?>
